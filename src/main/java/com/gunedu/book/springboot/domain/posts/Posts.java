63724b13aaae546c1204063616417a25489e14e6
pguhn9 <ddpgunhi23@naver.com>
2021-06-30 16:54:42 +0800

    mustache 등록 추가

--- src/main/java/com/gunedu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/gunedu/book/springboot/domain/posts/Posts.java
@@ -1,4 +1,5 @@
 package com.gunedu.book.springboot.domain.posts;
+import com.gunedu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
