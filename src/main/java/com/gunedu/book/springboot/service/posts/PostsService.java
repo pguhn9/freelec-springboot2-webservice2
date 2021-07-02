70f91aeb4076a75925435967ad96500151d6bb84
pguhn9 <ddpgunhi23@naver.com>
2021-07-02 18:20:49 +0800

    mustache 목록 수정 삭제 기능 추가

--- src/main/java/com/gunedu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/gunedu/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.gunedu.book.springboot.service.posts;
 
 import com.gunedu.book.springboot.domain.posts.Posts;
 import com.gunedu.book.springboot.domain.posts.PostsRepository;
+import com.gunedu.book.springboot.web.dto.PostsListResponseDto;
 import com.gunedu.book.springboot.web.dto.PostsResponseDto;
 import com.gunedu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.gunedu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +31,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllBy().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        // .map(posts -> new PostsListResponseDto(posts))
     }
 
 }
